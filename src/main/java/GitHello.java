b3641588cc9ed469753a2d941e5a46d6b83c554d
jiujiu1024 <3489314031@qq.com>
2022-11-10 16:25:25 +0800

    two hot-fix test

--- src/main/java/GitHello.java
+++ src/main/java/GitHello.java
@@ -3,5 +3,6 @@ public class GitHello {
         System.out.println("111111111111");
         System.out.println("222222222222");
         System.out.println("333333333333");
+        System.out.println("444444444444");
     }
 }
